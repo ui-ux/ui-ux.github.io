e202a192437fc49e872e93ab74191f8df93f22f1
Serhii Laptiev <ui-ux-developer@ukr.net>
2023-09-24 17:02:23 +0300

    added create pdf

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import * as jspdf from 'jspdf';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-profile',
@@ -7,4 +9,33 @@ import { Component } from '@angular/core';
 })
 export class ProfileComponent {
 
+  @ViewChild('content', { static: false }) content!: ElementRef;
+
+  public now = new Date();
+  sdata = this.now.toLocaleDateString();
+  timeData = this.now.getHours() + ' : ' + this.now.getMinutes();
+
+  constructor() {}
+
+  generatePDF() {
+    const data = document.getElementById('table');
+
+    html2canvas(data!).then(canvas => {
+      // Захоплюємо зображення з вмісту HTML-елемента
+      const imgData = canvas.toDataURL('image/png');
+
+      // Розмір PDF документу, можна налаштувати за потребою
+      const pdf = new jspdf.jsPDF('p', 'mm', 'a4');
+
+      // Визначення відступів
+      const marginX = 10; // Відступи зліва та справа (у міліметрах)
+      const marginY = 10; // Відступи зверху та знизу (у міліметрах)
+
+      // Додаємо зображення на PDF з відступами
+      pdf.addImage(imgData, 'PNG', marginX, marginY, 210 - 2 * marginX, 297 - 2 * marginY);
+
+      // Завантаження PDF-документа зі зображенням таблиці та відступами
+      pdf.save('report.pdf');
+    });
+  }
 }
